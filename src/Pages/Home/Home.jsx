4fd8face3ecca9ca940360e63ee0438bf9ed2f3a
Chynwe <echinwenduvictoriae@gmail.com>
2022-12-15 22:26:59 +0100

    second

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
     <div className='home bg-opacity-1 '>
       <div className='bg-black overlay'></div>
       <p className='intro text-white z-10 flex'> Hi! I'm Chynwe, and I'm a Software Developer 
-      <button>Contact me!</button>
       </p> 
+      <button>Contact me!</button>
       {/* <button>Contact me!</button> */}
     </div>
   )
